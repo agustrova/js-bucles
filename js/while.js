8655c0ac0c04e039caf61025c719bd11464fd868
agustrova <agustinatrova@gmail.com>
2023-09-20 20:26:45 -0300

    Cambio en el index2

--- js/while.js
+++ js/while.js
@@ -1,1 +1,5 @@
 console.log(`Archivo de bucle white cargado`)
+
+// gir fetch para preguntar si hay cambios en la nube
+// git push es para enviar los cambios en el local hacia la nube
+// git pull origin main
